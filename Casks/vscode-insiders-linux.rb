d314670b6e5f425089eaa434bd6be19fad25bc19
Josh Yorko <54248591+joshyorko@users.noreply.github.com>
2026-03-28 08:54:35 -0400

    Add VS Code Insiders Linux tap pipeline (#6)

---
+++ Casks/vscode-insiders-linux.rb
@@ -0,0 +1,70 @@
+cask "vscode-insiders-linux" do
+  arch intel: "x64"
+  os linux: "linux"
+
+  version "1.114.0,1774631137,00515ed0a37c"
+  sha256 x86_64_linux: "7b1debbd323f36b1ac87420f19bdb5915d3a33adb5191c429c81f7ba89914642"
+
+  url "https://github.com/joshyorko/homebrew-tools/releases/download/vscode-insiders-linux-1.114.0-1774631137-00515ed0a37c/vscode-insiders-linux-1.114.0-1774631137-00515ed0a37c.tar.gz"
+  name "Visual Studio Code - Insiders"
+  desc "Insiders build of Visual Studio Code packaged for Linux Homebrew"
+  homepage "https://code.visualstudio.com/insiders/"
+
+  livecheck do
+    skip "Updated by the tap's GitHub Actions workflow."
+  end
+
+  binary "VSCode-linux-x64/bin/code-insiders", target: "code-insiders"
+  binary "VSCode-linux-x64/bin/code-tunnel-insiders", target: "code-tunnel-insiders"
+  artifact "vscode-insiders-linux.desktop",
+           target: "#{Dir.home}/.local/share/applications/vscode-insiders-linux.desktop"
+  artifact "VSCode-linux-x64/resources/app/resources/linux/code.png",
+           target: "#{Dir.home}/.local/share/icons/hicolor/512x512/apps/vscode-insiders-linux.png"
+
+  preflight do
+    FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
+    FileUtils.mkdir_p "#{Dir.home}/.local/share/icons/hicolor/512x512/apps"
+
+    desktop_file = "#{staged_path}/vscode-insiders-linux.desktop"
+    File.write(desktop_file, <<~EOS)
+      [Desktop Entry]
+      Version=1.0
+      Type=Application
+      Name=Visual Studio Code - Insiders
+      GenericName=Text Editor
+      Comment=Code Editing. Redefined.
+      Exec=#{HOMEBREW_PREFIX}/bin/code-insiders %F
+      Icon=#{Dir.home}/.local/share/icons/hicolor/512x512/apps/vscode-insiders-linux.png
+      Terminal=false
+      StartupNotify=false
+      StartupWMClass=Code - Insiders
+      Categories=TextEditor;Development;IDE;
+      MimeType=text/plain;inode/directory;x-scheme-handler/vscode-insiders;
+      Keywords=vscode;code;editor;insiders;
+      Actions=new-empty-window;
+
+      [Desktop Action new-empty-window]
+      Name=New Empty Window
+      Exec=#{HOMEBREW_PREFIX}/bin/code-insiders --new-window %F
+      Icon=#{Dir.home}/.local/share/icons/hicolor/512x512/apps/vscode-insiders-linux.png
+    EOS
+  end
+
+  zap trash: [
+    "#{Dir.home}/.local/share/applications/vscode-insiders-linux.desktop",
+    "#{Dir.home}/.local/share/icons/hicolor/512x512/apps/vscode-insiders-linux.png",
+  ]
+
+  caveats <<~EOS
+    Launch the editor with:
+      code-insiders
+
+    Tunnel CLI is also available:
+      code-tunnel-insiders
+
+    App launcher installed for immutable/atomic desktops:
+      #{Dir.home}/.local/share/applications/vscode-insiders-linux.desktop
+
+    If it doesn't appear in your app grid immediately, log out and back in.
+  EOS
+end
